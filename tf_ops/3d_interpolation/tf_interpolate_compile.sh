1876878b806b41d7fc865f2625964b65e9e2eb4f
Yijie Xu <YIJIE.XU.10@UCL.AC.UK>
2022-02-19 18:12:43 +0200

    Update tf_interpolate_compile.sh

--- tf_ops/3d_interpolation/tf_interpolate_compile.sh
+++ tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -4,4 +4,4 @@ CUDA_LIB='/usr/local/cuda-11.0/lib64'
 TF_CFLAGS=$(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))')
 TF_LFLAGS=$(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))')
 
-g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC $TF_CFLAGS $TF_LFLAGS -I$CUDA_INC -lcudart -L$CUDA_LIB -O2 -D_GLIBCXX_USE_CXX11_ABI=1
+g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC $TF_CFLAGS $TF_LFLAGS -I$CUDA_INC -lcudart -L$CUDA_LIB -O2 -D_GLIBCXX_USE_CXX11_ABI=0
